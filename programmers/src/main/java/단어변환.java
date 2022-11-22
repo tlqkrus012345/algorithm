29611d753a01f5f81198b2fa3d447e7ca3ef2162
tlqkrus012345 <tlqkrus012345@naver.com>
2022-11-22 22:11:03 +0900

    [programmers] 단어변환, 피로도

---
+++ programmers/src/main/java/단어변환.java
@@ -0,0 +1,35 @@
+public class 단어변환 {
+    int answer = 0;
+    boolean[] visit;
+    public int solution(String begin, String target, String[] words) {
+        visit = new boolean[words.length];
+        dfs(begin, target, words, 0);
+        return answer;
+    }
+    public void dfs(String begin, String target, String[] words, int depth) {
+        if(begin.equals(target)) {
+            answer = depth;
+            return;
+        }
+        for(int i=0; i<words.length; i++) {
+            int cnt = 0;
+            if(visit[i]) continue;
+            for(int j=0; j<words[i].length(); j++) {
+                if (begin.charAt(j) == words[i].charAt(j)) {
+                    cnt++;
+                }
+            }
+            if(cnt == begin.length()-1) {
+                visit[i] = true;
+                dfs(words[i], target, words, depth+1);
+                visit[i] = false;
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        단어변환 ps = new 단어변환();
+        System.out.println(ps.solution("hit","cog",new String[]{"hot","dog","lot","log","cog"}));
+    }
+}
---
+++ programmers/src/main/java/피로도.java
@@ -0,0 +1,23 @@
+public class 피로도 {
+    boolean[] visit;
+    int answer = 0;
+    public int solution(int k, int[][] dungeons) {
+        visit = new boolean[dungeons.length];
+        dfs(dungeons, k, 0);
+        return answer;
+    }
+    public void dfs(int[][] dungeons, int k, int depth) {
+        for(int i=0; i< dungeons.length; i++) {
+            if(!visit[i] && k >= dungeons[i][0]) {
+                visit[i] =true;
+                dfs(dungeons, k - dungeons[i][1], depth+1);
+                visit[i] = false;
+            }
+        }
+        answer = Math.max(answer, depth);
+    }
+    public static void main(String[] args) {
+        피로도 ps = new 피로도();
+        System.out.println(ps.solution(80,new int[][]{{80,30},{40,30},{20,10}}));
+    }
+}
