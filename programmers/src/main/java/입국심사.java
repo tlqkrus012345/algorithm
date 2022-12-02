ad84522e124c6ab2745217a68c3a4864312e500e
tlqkrus012345 <tlqkrus012345@naver.com>
2022-12-02 19:52:04 +0900

    [programmers] 입국심사

---
+++ programmers/src/main/java/입국심사.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class 입국심사 {
+    public long solution(int n, int[] times) {
+        Arrays.sort(times);
+        long start = 0;
+        long end = (long) n * times[times.length - 1];
+        long result = 0;
+        while(start<=end){
+            long total = 0;
+            long mid = (start+end)/2;
+            for(int i=0; i<times.length; i++){
+                total += mid/times[i] ;
+            }
+
+            if(total < n){
+                start = mid +1;
+
+            }
+            else{
+                end = mid-1;
+                result = mid;
+            }
+
+        }
+        return result;
+
+    }
+
+    public static void main(String[] args) {
+        입국심사 ps = new 입국심사();
+        System.out.println(ps.solution(6, new int[]{7,28}));
+    }
+}
